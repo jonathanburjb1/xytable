c53f8571df93f5ab2124925a75a5144228341541
Jonathan Burpeau <jonathan.burpeau@micross.com>
2025-07-29 16:51:07 -0400

    updated start script to set linuxcnc priorities to the highest possible

--- scripts/start_lcnc.sh
+++ scripts/start_lcnc.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 # script to start linuxcnc with the correct configuration
 
+gio launch /home/micross/Desktop/xytable.desktop
+#sleep 2
 
-linuxcnc ~/linuxcnc/configs/xytable/xytable.ini
+#set the niceness (priority) of the linuxcnc processes to -20 to prevent watchdog timeouts
+ps axo pid,comm | grep linuxcnc | grep -o "[0-9]*" | while read -r line ; do
+	sudo renice -n -20 -p $line
+done
